cd929c3e118f9432c5752c934751f117044c1eb5
LuisRivaldo <rivaldo.rodrigueza@uanl.edu.mx>
2025-10-05 02:13:15 -0600

    tercer commit

---
+++ app/theme-providers.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React, { ReactNode } from 'react'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { Toaster } from 'sonner'
+
+const queryClient = new QueryClient()
+
+const ThemeProvider = ({ children }: {children: ReactNode}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+        <Toaster 
+            position='bottom-right'
+            richColors
+        />
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default ThemeProvider
